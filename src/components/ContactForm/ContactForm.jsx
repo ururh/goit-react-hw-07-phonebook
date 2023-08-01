79b7cf4f396506b21cca5fb634533369f5ef7512
ururh <58848421+ururh@users.noreply.github.com>
2023-08-01 11:18:30 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,77 +1,75 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContactsThunk, getContactsThunk } from 'redux/contactsThunk';
 import { AddButton, FormInfo, Input, LabelText } from './ContactForm.module';
-
 
 export const ContactForm = () => {
   const dispatch = useDispatch();
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  useEffect(() => {
-    dispatch(getContactsThunk());
-  }, [dispatch]);
-  const handleChange = evt => {
+  const handleChange = (evt) => {
     const { name, value } = evt.target;
     name === 'name' ? setName(value) : setNumber(value);
   };
+
   const reset = () => {
     setName('');
     setNumber('');
   };
-  const contacts = useSelector(state => state.contacts.items);
 
- const handleSubmit= e => {
-        const notifly = () => alert(`${name} is alredy in contacts`);
-        const contact = {
-          name: name,
-          phone: number,
-        };
-        e.preventDefault();
-        if (
-          contacts.some(
-            value => value.name.toLocaleLowerCase() === name.toLocaleLowerCase()
-          )
-        ) {
-          notifly();
-        } else {
-          dispatch(addContactsThunk(contact));
-          reset();
-        }
-      }
+  const contacts = useSelector((state) => state.contacts.items);
+
+  const handleSubmit = (e) => {
+    const notifly = () => alert(`${name} is already in contacts`);
+    const contact = { name, phone: number };
+    e.preventDefault();
+    if (
+      contacts.some(
+        (value) => value.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+      )
+    ) {
+      notifly();
+    } else {
+      dispatch(addContactsThunk(contact));
+      reset();
+    }
+  };
+
+  useEffect(() => {
+    dispatch(getContactsThunk());
+  }, []);
 
   return (
     <FormInfo onSubmit={handleSubmit}>
-      
-        <LabelText>
-          Name
-        </LabelText>
-        <Input
-          value={name}
-          onChange={handleChange}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+      <LabelText>
+        Name
+      </LabelText>
+      <Input
+        value={name}
+        onChange={handleChange}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
 
-        <LabelText>
-          Number
-        </LabelText>
-        <Input
-          value={number}
-          onChange={handleChange}
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-        <AddButton type="submit">
-          Add contact
-        </AddButton>
+      <LabelText>
+        Number
+      </LabelText>
+      <Input
+        value={number}
+        onChange={handleChange}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <AddButton type="submit">
+        Add contact
+      </AddButton>
     </FormInfo>
   );
 };
--- src/service/contactsAPI.js
+++ src/service/contactsAPI.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
-const BASEURL = 'https://64c4d51367cfdca3b661075c.mockapi.io';
+
 export const contactAPI = axios.create({
-  BaseURL: 'https://64c4d51367cfdca3b661075c.mockapi.io/api/v1',
+  baseURL: 'https://64c4d51367cfdca3b661075c.mockapi.io/api/v1',
 });
 
 export const getContacts = async () => {
-  const { data } = await contactAPI.get(`${BASEURL}/contacts`);
+  const { data } = await contactAPI.get(`/contacts`);
   return data;
 };
 
 export const addContacts = async contact => {
-  const { data } = await contactAPI.post(`${BASEURL}/contacts`, contact);
+  const { data } = await contactAPI.post(`/contacts`, contact);
   return data;
 };
 
 export const delContacts = async id => {
-  const { data } = await contactAPI.delete(`${BASEURL}/contacts/${id}`);
+  const { data } = await contactAPI.delete(`/contacts/${id}`);
   return data;
 };
